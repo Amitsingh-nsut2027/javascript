c6c9824c5f75e56e7ae37f6d3b9198242d081619
Amitsingh-nsut2027 <as8840677@gmail.com>
2025-07-25 21:42:47 +0530

    datatypes commit

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+"use strict"; //Treat all js codes as newer version 
+
+
+// alert(3 + 3)  we are use node.js not browser
+
+console.log(3+3); 
+console.log("hitesh")
+
+let name = "Amit singh"
+let age = 21
+let isLoggedIn = false 
+
+
