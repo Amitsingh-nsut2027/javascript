2fa67849f6f76816a4ccb8dd0d12b86bd5bf3245
Amitsingh-nsut2027 <as8840677@gmail.com>
2025-07-25 22:03:30 +0530

    final commit in conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,13 @@
+// let score = true
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isLoggedIn = 5255
+
+let booleanIsLoogedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoogedIn);
