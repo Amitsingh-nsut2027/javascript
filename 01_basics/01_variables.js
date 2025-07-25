8677daada4423311a8160af796bf6e5cc7abe180
Amitsingh-nsut2027 <as8840677@gmail.com>
2025-07-25 21:25:49 +0530

    first commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "as8840677@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+// accountId = 2            not allowed 
+accountEmail = "hc@hc.com"
+accountpassword = "212121"
+accountCity = "bengluru"
+
+console.log(accountId);
+
+console.table([accountId , accountEmail , accountPassword, accountCity])
