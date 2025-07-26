dffebe404ee7f1638edfd5e0811581f395af165c
Amitsingh-nsut2027 <as8840677@gmail.com>
2025-07-26 20:25:20 +0530

    datatypes

---
+++ 01_basics/datay-types-summary.js
@@ -0,0 +1,28 @@
+// Primitive
+
+/* 7 ctaegories types : String ,Number , Boolean , null , undefined, Symbol , BigInt */
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+
+// Refrence (Non-primitive)-
+// Array , objects , Functions
+
+const heros = ["shaktiman" , "naagraj" , "doga"]
+
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function() {
+    console.log("Hello ! Amit ");
+    
+}
+
+console.log(typeof myFunction);
+console.log(typeof myObj);
+console.log(typeof heros);
