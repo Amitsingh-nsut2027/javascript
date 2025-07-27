29764749d1317d9fe1429091be2c85a0580c0d74
Amitsingh-nsut2027 <as8840677@gmail.com>
2025-07-27 23:50:13 +0530

    Done with maths and nums

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,25 @@
+// const score = 400
+// const balance = new Number(100)
+// console.log(balance);
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ++++++++++++++++++++++++   Maths ++++++++++++++
+
+console.log(Math);
+
+console.log(Maths.random());
+console.log((Maths.random()*10 +1));
+console.log((Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Maths.random() * (max - min + 1 )+ min);
