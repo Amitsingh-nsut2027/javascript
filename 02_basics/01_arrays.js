d257715021c1ff64baa2b1160a87f2365e13f1b0
Amitsingh-nsut2027 <as8840677@gmail.com>
2025-07-28 00:39:38 +0530

    Arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,14 @@
+const myArr = [0,1,2,3,4,6]
+
+// const myHero = ["UdaypalSingh" , "Father"]
+
+// const myArr2 = new Array(1,3,5,6,7)
+
+// console.log(myArr[1]);
+
+
+// Arrays methids
+
+
+myArr.push(5)
+console.log(myArr);
