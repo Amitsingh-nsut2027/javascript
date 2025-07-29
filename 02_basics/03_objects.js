8e2dafe2fe6cff2fe565b6a832e7dda2b613b11b
Amitsingh-nsut2027 <as8840677@gmail.com>
2025-07-30 00:38:39 +0530

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,10 @@
+// Object literals 
+
+const objUser = {
+    name : "Amit Singh",
+    age: 21,
+    location: "Delhi",
+    email: "As8840677@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
