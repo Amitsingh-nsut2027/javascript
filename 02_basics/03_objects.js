b0d065f07a0dea45f3ef2bbf435b26a341c44f9f
Amitsingh-nsut2027 <as8840677@gmail.com>
2025-08-01 19:20:12 +0530

    object access and function

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,7 +1,13 @@
 // Object literals 
 
+
+const mySym = Symbol("key1")
+
+
 const objUser = {
     name : "Amit Singh",
+    "full name ": "AMit SIngh Bhati",
+    [mySym ]: "myKey1",
     age: 21,
     location: "Delhi",
     email: "As8840677@gmail.com",
@@ -9,7 +15,27 @@ const objUser = {
     lastLoginDays: ["Monday", "Saturday"],
     // console.log(name),
 }
-console.log(objUser.name)
-console.log(objUser.age)
+// console.log(objUser.name)
+// console.log(objUser["email"])
+// console.log(objUser["full name "]);
+// console.log(objUser [mySym]);
 
-// new add branch 
+// objUser.email = "Amit2004@gml.xom"
+// Object.freeze(objUser)
+// objUser.location = "jaipur"
+// console.log(objUser);
+
+
+ objUser.greeting = function(){
+    console.log("hello amit singh");
+ }
+ 
+ 
+
+ objUser.greeting2 = function()
+    {
+    console.log(`Hello js user, ${this.name}`);
+    
+ }
+console.log(objUser.greeting());
+console.log(objUser.greeting2());
