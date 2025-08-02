63a5b4855285274d32b92315df9864f0a8e08373
Amitsingh-nsut2027 <as8840677@gmail.com>
2025-08-02 19:41:29 +0530

    Scopes

---
+++ 03_basics/03_scoops.js
@@ -0,0 +1,12 @@
+function one(){
+    const username = "AMit singh"
+
+    function two(){
+        const website = "Youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);
+    // two()
+}
+one()
