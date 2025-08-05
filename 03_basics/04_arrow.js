9f52e5894609e1c277c97eb3b8ed943ff8db2442
Amitsingh-nsut2027 <as8840677@gmail.com>
2025-08-05 22:43:11 +0530

    Arrow funcn

--- 03_basics/04_arrow.js
+++ 03_basics/04_arrow.js
@@ -1,1 +1,41 @@
- // arrows in js 
+//  // arrows in js 
+
+//  const user = {
+
+//     username : "Amit singh",
+//     price : 999,
+
+//     welcomeMessage: function(){
+//         console.log(`${this.username} , welcome to website`);
+//         console.log(this);
+        
+        
+//     }
+//  }
+ 
+//  user.welcomeMessage()
+//  user.username = "sam"
+//  user.welcomeMessage()
+
+//  console.log(this);
+ 
+
+// function chai(){
+//     let username =  "amit singh"
+//     console.log(this.username);
+    
+// }
+// chai()
+
+// const chai =  () =>{
+//     let usename = "Amit singh"
+//     console.log(this.username);
+    
+// }
+// chai() 
+
+const addTwo = (num1 , num2) =>{
+    return num1+num2;
+}
+
+console.log(addTwo(2,4));
