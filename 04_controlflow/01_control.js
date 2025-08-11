cd263907e911d09cb4321c63acd0d583cb08a728
Amitsingh-nsut2027 <as8840677@gmail.com>
2025-08-11 17:25:06 +0530

    Controlflow

---
+++ 04_controlflow/01_control.js
@@ -0,0 +1,76 @@
+
+
+// if (condition) {
+
+// }
+
+const isUserloggedIn = true 
+
+// if (isUserloggedIn) {
+    
+// }
+
+// if (3 != 2){
+//     console.log("true");
+// }
+
+//        console.log("Done with first")
+
+
+// if( 2 !== "2"){
+//     console.log("true")
+// }
+
+// const score = 40
+
+// if(score>20){
+//     let  power = "fly"
+
+//     console.log(`user power:  ${power}`);
+// }
+
+// console.log(`user power: ${power}`)
+
+
+
+ 
+// const balance  = 1000
+//  if(balance > 500) console.log("true");
+
+// if (balance < 500) {
+//     console.log(
+//         "greater than 500 "
+//     );
+    
+// }
+// else if(balance < 750){
+//     "less than 750"
+
+// }
+
+// else if(balance < 900){
+//     console.log("less than 900");
+    
+// }
+// else
+// {
+//     console.log(`gretater than ${balance}`);
+    
+// }
+
+
+const userLoggedIn = true 
+
+const debitCard = true
+const LoggedInFromFoogle = true
+
+const LoggedInFromEmail = false
+if (userLoggedIn && debitCard) {
+    console.log("Allow to buy courses");
+    
+}
+
+if(LoggedInFromEmail || LoggedInFromFoogle || true){
+    console.log("user looged in");
+    
+}
---
+++ 04_controlflow/truthy.js
@@ -0,0 +1,19 @@
+const userEmail = "amitsingh2004@gmail.com"
+
+if(userEmail){
+    console.log("got user email");
+    
+}
+else {
+    console.log("Don't have user emial");
+    
+}
+
+// falsy value 
+ // false , 0,-0, BigInt, "", null, undefined ,NaN
+
+ //truthy
+
+ // "0", 'false', " ", [],{}, function(){}
+
+  
