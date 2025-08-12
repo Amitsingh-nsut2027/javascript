3b26e4f9b9494a211a09f27dada37ac1c60ee7d5
Amitsingh-nsut2027 <as8840677@gmail.com>
2025-08-12 19:45:48 +0530

    of

---
+++ 05_iterations/03_of.js
@@ -0,0 +1,31 @@
+// for of
+
+// const arr = [1,2,3,4,5,]
+
+
+// for(const iterator of arr){
+//     console.log(iterator);
+    
+// // }
+
+// const greetings = "Hello World !"
+
+// for(const greet of greetings){
+//     console.log(`Each char is ${greet}`);
+    
+// }
+
+// Maps
+
+const map = new Map()
+map.set('IN', 'India')
+map.set('Fr', 'france')
+map.set('Ru', 'russia')
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    console.log(key ,':-', value);
+    
+    
+}
